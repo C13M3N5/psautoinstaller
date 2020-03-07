13a756698325dcd394ca5394b0e5a3a42f662ae4
Clemens <54906921+C13M3N5@users.noreply.github.com>
2020-03-07 14:00:39 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,11 +1,19 @@
-
+$all = $true
 if($all){
   $socialmedia = $true
   $games = $true
+  $coding = $true
+  $systeminfo = $true
 }
 
 Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+## 7zip
+choco install 7zip -y
+
+## cccleaner
+choco install ccleaner -y
 
 ## Install Notepad++
 choco install notepadplusplus -y
@@ -14,39 +22,53 @@ choco install notepadplusplus -y
 choco install winscp -y
 
 ## Office 365
-choco install office365proplus
+choco install office365proplus -y
 
 ## Keepass
-choco install keepass
-choco install keepass-keepasshttp
+choco install keepass -y
+choco install keepass-keepasshttp -y
 
 ## Chrome
-choco install chromelpass-chrome
+choco install googlechrome -y
+choco install chromelpass-chrome -y
+choco install adblockpluschrome -y
+
+## Firefox
+choco install firefox -y
+choco install adblockplusfirefox -y
+choco install adblockplusie -y
 
 ## Putty
-choco install putty
+choco install putty -y
 
 ## Paint.net
-choco install paint.net
+choco install paint.net -y
 
 ## Games
 if($steam -or $games){
   ## Steam Client
-  choco install steam
+  choco install steam -y
 }
 if($teamspeak -or $games)
   ## Teamspeak Client
-  choco install teamspeak
+  choco install teamspeak -y
 }
 
 ## Social Media
 # Twitch
 if($twitch -or $soicalmedia){
-  choco install twitch
+  choco install twitch -y
 }
 
+if($vscode -or $coding){
+  choco install vscode -y
+}
 
+if($windisk -or $coding){
+  choco install win32diskimager -y
+}
 
-
-
+if($diskinfo -or $systeminfo){
+  choco install crystaldiskinfo -y
+}  
 

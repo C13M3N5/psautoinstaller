adf5627ebc1b2724a02c95f0b9b4b5fe433a8fc3
Clemens <54906921+C13M3N5@users.noreply.github.com>
2020-03-08 13:42:05 +0100

    Add files via upload

--- install.ps1
+++ install.ps1
@@ -1,94 +1,122 @@
-$all = $true
-if($all){
-  $socialmedia = $true
-  $games = $true
-  $coding = $true
-  $systeminfo = $true
+<# 
+.SYNOPSIS 
+	Installiert Programme über Chocolatey
+.DESCRIPTION 
+	Checkt ob Chocolatey installiert ist dann wird es installiert.
+	Dann werden die ausgewählten Programme in der CSV-Datei installiert oder deinstalliert
+.EXAMPLE 
+	install.ps1 
+#>
+
+# Get the programm.csv
+$csvdatapath = $PSScriptRoot + "\programs.csv"
+
+
+
+function Intall-Programm($program){
+	$checkprogram = $chocolistinstall | Where-object { $_.ToLower().StartsWith($program.ToLower()) }
+	if($checkprogram -eq $null){
+		choco install $program -y
+		# Fehlercode von Choco auswerten
+		if($LASTEXITCODE){
+			return 1
+		}else{
+			return 0
+		}
+	}
+	return 2
+
 }
 
-## Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
-
-## 7zip
-choco install 7zip -y
-
-## cccleaner
-choco install ccleaner -y
-
-## Install Notepad++
-choco install notepadplusplus -y
-
-## Install WinSCP
-choco install winscp -y
-
-## Office 365
-choco install office365proplus -y
-
-## Keepass
-choco install keepass -y
-choco install keepass-keepasshttp -y
-
-## Chrome
-choco install googlechrome -y
-choco install chromelpass-chrome -y
-choco install adblockpluschrome -y
-
-## Firefox
-choco install firefox -y
-choco install adblockplusfirefox -y
-choco install adblockplusie -y
-
-## Putty
-choco install putty -y
-
-## Paint.net
-choco install paint.net -y
-
-## Games
-if($steam -or $games){
-  ## Steam Client
-  choco install steam -y
-}
-if($teamspeak -or $games){
-  ## Teamspeak Client
-  choco install teamspeak -y
-}
-if($uplay -or $games){
-  ## uplay Client
-  choco install uplay -y
-}
-if($discord -or $games){
-  ## discord Client
-  choco install discord -y
-}
-if($origin -or $games){
-  ## origin Client
-  choco install origin -y
-}
-if($playnite){
-  ## playnite Client
-  choco install playnite -y
-}
-choco install 
-
-
-## Social Media
-# Twitch
-if($twitch -or $soicalmedia){
-  choco install twitch -y
+function Unintall-Programm($program){
+	$checkprogram = $chocolistinstall | Where-object { $_.ToLower().StartsWith($program.ToLower()) }
+	if($checkprogram -ne $null){
+		choco uninstall $program -y --remove-dependencies
+		# Fehlercode von Choco auswerten
+		if($LASTEXITCODE){
+			return 1
+		}else{
+			return 0
+		}
+	}
+	return 2
 }
 
-if($vscode -or $coding){
-  choco install vscode -y
+
+
+if(!(Test-Path $csvdatapath)){
+	"CSV Datei nicht gefunden! Bitte im Verzeichnis des Skripts ablegen."
+	exit
 }
 
-if($windisk -or $coding){
-  choco install win32diskimager -y
+$testchoco = powershell choco -v
+if(-not($testchoco)){
+    Write-Output "Seems Chocolatey is not installed, installing now"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+else{
+    Write-Output "Chocolatey Version $testchoco is already installed"
 }
 
-if($diskinfo -or $systeminfo){
-  choco install crystaldiskinfo -y
-}  
+# CSV-Datei auslesen
+$csvdata = Import-CSV $csvdatapath -delimiter ";"
 
-## Autoupdate with Task in Windows
-choco install choco-upgrade-all-at-startup -y
+# Alle installierten Pakete auslesen
+$chocolistinstall = choco list -lo
+
+#Array für den späteren CSV Export anlegen
+$outarray = @()
+
+foreach($row in $csvdata){
+	$install = $row.install
+	$programm = $row.programm
+	$description = $row.description
+	$timestamp = Get-Date -Format "dd.MM.yyyy-HH:mm"
+	if($install -eq 1){
+		$installreturn = Intall-Programm $programm 
+		if($installreturn -eq 0){
+			$status = $timestamp + " - Erfolgreich installiert"
+		}elseif($installreturn -eq 1){
+			$status = $timestamp + " - Fehler bei der Installation"
+		}elseif($installreturn -eq 2){
+			$status = $timestamp + " - schon installiert"
+		}
+	
+	}elseif($install -eq 0){
+		$installreturn = Unintall-Programm $programm 
+		if($installreturn -eq 0){
+			$status = $timestamp + " - Erfolgreich deinstalliert"
+		}elseif($installreturn -eq 1){
+			$status = $timestamp + " - Fehler bei der Deinstallation"
+		}elseif($installreturn -eq 2){
+			$status = $timestamp + " - nicht installiert"
+		}
+		
+	}else{
+		$status = $timestamp + " - Keine Auswahl für install definiert"
+	
+	}
+	$outarray += New-Object PsObject -property @{
+		'programm' = $programm
+		'install' = $install
+		'description' = $description
+		'status' = $status
+		
+		
+	}
+
+}
+
+#export to .csv file
+$outarray | Select-Object "programm", "install", "description", "status" | export-csv $csvdatapath -delimiter ";" -NoTypeInformation
+
+#$officepakt = "7zip","notepadplusplus","office365proplus","googlechrome","paint.net","firefox","adblockpluschrome","adblockplusfirefox","adblockplusie"
+#$keepasspakt = "keepass","keepass-keepasshttp","chromelpass-chrome"
+#$codingpakt = "notepadplusplus","winscp","putty","vscode","win32diskimager"
+#$gamingpakt = "steam","teamspeak","discord","uplay","battle.net","Origin" 
+#$socialpakt = "twitch"
+#$systempakt = "7zip","ccleaner","crystaldiskinfo"
+
+
+
+
